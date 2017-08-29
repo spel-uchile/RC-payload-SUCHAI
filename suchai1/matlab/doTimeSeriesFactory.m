f1e97cd77bd1b756195b588bc6c9bcc6077d7a0d
joseogalde <jose.ogalde@ing.uchile.cl>
2017-08-29 20:06:31 -0300

    set the 'filtered' timeseires to be equal than 'raw' series because there was some problems with the filtering process for the telemetry downloaded. In the future the filtered series should be considered in normal use

--- suchai1/matlab/doTimeSeriesFactory.m
+++ suchai1/matlab/doTimeSeriesFactory.m
@@ -2,7 +2,7 @@
 % files and modifies and saves them in MAT-files for further time series
 % creation.
 
-prefix ='2016_18_05';
+prefix ='2017_08_29';
 parserFolder = './parser';
 fixtureFolder = strcat(parserFolder,'/', prefix);
 parserFiles = dir(fixtureFolder);
@@ -18,7 +18,7 @@ fixtureIndexForOutputFiles = 0;
 for i = 1 : length(parserFiles)
     saveFolder = strcat('./mat/ts/', prefix);
     fixtureIndexForOutputFiles = fixtureIndexForOutputFiles + 1;
-
+    
     if i ~= fixtureIndexForInputParserFiles
         outputFixture{fixtureIndexForOutputFiles} = strcat(fixtureFolder,'/',...
             parserFiles{fixtureIndexForOutputFiles});
@@ -38,9 +38,12 @@ for i = 1 : length(parserFiles)
         save(strcat(saveFolder, '/',raw.Name,'.mat'),'raw','-v7.3');
         
         %filtered timeserie
-        filtered = timeSeriesFactory(fsignal, 'filtered', inputFixture,...
+        %         filtered = timeSeriesFactory(fsignal, 'filtered', inputFixture,...
+        %             outputFixture{fixtureIndexForOutputFiles}, sampCoeff);
+        % save(strcat(saveFolder, '/', filtered.Name,'.mat'),'filtered','-v7.3');
+        filtered = timeSeriesFactory(fsignal, 'raw', inputFixture, ...
             outputFixture{fixtureIndexForOutputFiles}, sampCoeff);
-        save(strcat(saveFolder, '/', filtered.Name,'.mat'),'filtered','-v7.3');
+        save(strcat(saveFolder, '/','filtered_2937.9922Hz.mat'),'filtered','-v7.3');
         
         %simulation timeserie
         simulation = timeSeriesFactory(fsignal, 'simulink', inputFixture, ...
@@ -54,6 +57,11 @@ for i = 1 : length(parserFiles)
         Parameters.dacMaxVoltage = 3.3;
         Parameters.dacMinvoltage = 0;
         Parameters.oversamplingCoeff = 4;
+        Parameters.buffLen = 200;
+        Parameters.sampledValuesPerRound = floor(Parameters.buffLen / Parameters.oversamplingCoeff); %NOT USED REALLY
+        Parameters.rounds = 3;%NOT USED REALLY
+        Parameters.nWaitingUnits = 350; %NOT USED REALLY
+        Parameters.nonSampledValuesPerRound = 500;%NOT USED REALLY
         theoretical = timeSeriesFactory(fsignal, 'theoretical', Parameters);
         save(strcat(saveFolder, '/', theoretical.Name,'.mat'),'theoretical','-v7.3');
     end
