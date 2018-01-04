d398df3b7140137a128c3ba21e928fe032c01549
joseogalde <jose.ogalde@ing.uchile.cl>
2018-01-04 13:10:23 -0300

    added circuit-like variables (i,v,p) and langevin equations variables (stored, dissipation, injection) to the timeseries building process

--- suchai1/matlab/makeExperimentalSeries.m
+++ suchai1/matlab/makeExperimentalSeries.m
@@ -1,4 +1,4 @@
-function tsCollection = makeExperimentalSeries(inputStruct, outputStruct, varargin)
+function tsCollection= makeExperimentalSeries(inputStruct, outputStruct, varargin)
 
 if nargin < 5
     error('not enough arguments');
@@ -12,43 +12,69 @@ dampingRateHz = R_ohm*C_farads;
 
 %% Measurements (Vin and Vout)
 disp(strcat('Building Vin with freq ', num2str(freqSignalHz), ' Hz'));
-vinTimeSeries = createVin(inputStruct, freqSignalHz, oversamplingCoeff);
+vIn = createVin(inputStruct, freqSignalHz, oversamplingCoeff);
 
 disp(strcat('Building Vout with freq ', num2str(freqSignalHz), ' Hz'));
-voutTimeSeries = createVout(outputStruct, vinTimeSeries.Time);
+vOut = createVout(outputStruct, vIn.Time);
 
-%% Resistor
-vR = timeseries((vinTimeSeries.Data - voutTimeSeries.Data),...
-    voutTimeSeries.Time, 'name', 'vR');
+%% Circuit equation timeseries
+%Resistor
+vR = timeseries((vIn.Data - vOut.Data),...
+    vOut.Time, 'name', 'Vr');
 vR.DataInfo.Units = 'V';
-iR = timeseries((vR.Data).*(1000/R_ohm), voutTimeSeries.Time, ...
-    'name', 'injectedPower');
+iR = timeseries((vR.Data).*(1000/R_ohm), vOut.Time, ...
+    'name', 'Ir');
 iR.DataInfo.Units = 'mA';
-pR = timeseries((iR.Data).*(vR.Data),'name', 'dissipatedPower');
+pR = timeseries((iR.Data).*(vR.Data),vOut.Time,'name', 'Pr');
 pR.DataInfo.Units = 'mW';
 
-%% Capacitor
-x = voutTimeSeries.Data;
-t = voutTimeSeries.Time.Data;
-dx = x(2:end) - x(1:end-1);
+% Capacitor
+v = vOut.Data;
+t = vOut.Time;
+dv = v(2:end) - v(1:end-1);
 dt = t(2:end) - t(1:end-1);
-dValue = dx./dt;
-dValue = [0 dValue]; % add a zero
-ic = timeseries((1000*C_farads).*dValue,t,'name', 'iC');
-ic.DataInfo.Units = 'mA';
+dValue = dv./dt;
+dValue = [0; dValue]; % add a zero
+iC = timeseries((1000*C_farads).*dValue, vOut.Time,'name', 'Ic');
+iC.DataInfo.Units = 'mA';
 
-x2 = voutTimeSeries.Data.*voutTimeSeries.Data;
-t = voutTimeSeries.Time.Data;
-dx = x2(2:end) - x2(1:end-1);
+v2 = vOut.Data.*vOut.Data;
+t = vOut.Time;
+dv2 = v2(2:end) - v2(1:end-1);
 dt = t(2:end) - t(1:end-1);
-dValue = dx./dt;
-dValue = [0 dValue]; % add a zero
-pC = timeseries((1000*(C_farads/2)).*dValue,t,'name', 'pC');
+dValue2 = dv2./dt;
+dValue2 = [0; dValue2]; % add a zero
+pC = timeseries((1000*(C_farads/2)).*dValue2, vOut.Time,'name', 'Pc');
 pC.DataInfo.Units = 'mW';
 
-%% Injected Power
-pIn = timeseries((vinTimeSeries.Data .* iR.Data),...
-    voutTimeSeries.Time, 'name', 'pIn');
+%Injected Power
+pIn = timeseries((vIn.Data .* iR.Data),...
+    vOut.Time, 'name', 'Pin');
 pIn.DataInfo.Units = 'mW';
-tsCollection = tscollection({vinTimeSeries, voutTimeSeries, pIn, pR, pC, iR, iC, vR});
+
+deltaPower = timeseries((pIn.Data - (pR.Data + pC.Data)),...
+    vOut.Time, 'name', 'DeltaPower');
+deltaPower.DataInfo.Units = 'mW';
+
+%% Langevin equation variables
+% Resistor
+dissFactorLangevin = v2.*dampingRateHz;
+langevinDissipated = timeseries(dissFactorLangevin,vOut.Time,'name','langevinDissipated');
+langevinDissipated.DataInfo.Units = 'V^2 Hz';
+
+% Capacitor
+langevinStored = timeseries((dValue2)./2, vOut.Time,'name', 'langevinStored');
+langevinStored.DataInfo.Units = 'V^2 Hz';
+
+%Injected Power
+langevinInjected = timeseries(dampingRateHz.*(vIn.Data .* vOut.Data),...
+    vOut.Time, 'name', 'langevinInjected');
+langevinInjected.DataInfo.Units = 'V^2 Hz';
+
+langevinDeltaPower = timeseries((langevinInjected.Data - (langevinDissipated.Data + langevinStored.Data)),...
+    vOut.Time, 'name', 'langevinDeltaPower');
+langevinDeltaPower.DataInfo.Units = 'V^2 Hz';
+
+tsCollection = tscollection({vIn, vOut, vR, iR, iC, pR, pC, pIn, deltaPower,...
+    langevinDissipated, langevinInjected, langevinDeltaPower});
 end
