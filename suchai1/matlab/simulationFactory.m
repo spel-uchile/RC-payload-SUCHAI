c96ebf21818d61f408299241393ca0119444a161
joseogalde <jose.ogalde@ing.uchile.cl>
2018-01-05 15:09:09 -0300

    extend the feature o the simulationFactory to construct timeseries of the circuit and the langevin equation

--- suchai1/matlab/simulationFactory.m
+++ suchai1/matlab/simulationFactory.m
@@ -5,9 +5,9 @@ if nargin < 2
 end
 
 freqCircuitHz = 92;
-R = 1210;
-C = 1 / (freqCircuitHz * 2 * pi * R);
-dampingRate = 1/ (R*C);
+R_ohm = 1210;
+C_farads = 1 / (freqCircuitHz * 2 * pi * R_ohm);
+dampingRate = 1/ (R_ohm*C_farads);
 
 option =  varargin{1};
 switch option
@@ -26,10 +26,15 @@ switch option
         valuesPerRound = length(tsInput.Data);
         tsInput = simulateSDtransfer(tsInput, valuesPerRound, nWaitingUnits, 'option2');
         tsOutput = simulateVout(tsInput);
-        tsInjPower = timeseries((dampingRate.* (tsInput.Data .* tsOutput.Data)), tsOutput.Time);
-        tsInjPower.Name = 'injectedPower';
-        tsInjPower.DataInfo.Units = 'V^2 Hz';
-        rawCollection = tscollection({tsInput, tsOutput, tsInjPower});
+        [vIn, vOut, vR, iR, iC, pR, pC, pIn, deltaPower] = ...
+            makeCircuitVariablesFromTimeSeries(tsInput, tsOutput, R_ohm, C_farads);
+        
+        [langevinDissipated, langevinStored, langevinInjected, langevinDelta ] = ...
+            makeLangevinVariablesFromTimeSeries( vIn, vOut, R_ohm, C_farads );
+        
+        rawCollection = tscollection({vIn, vOut, vR, iR, iC, pR, pC, pIn, deltaPower,...
+            langevinDissipated, langevinInjected, langevinStored, langevinDelta});
+        
         buffLen = length(rawCollection.Vin.Data);
         [indexes, ~, ~] = findSState('simple', rawCollection.Vout.Data, buffLen);
         tsCollection = filterCollection(rawCollection, indexes, buffLen);
@@ -56,11 +61,15 @@ switch option
         nWaitingUnits = Parameters.nWaitingUnits;
         valuesPerRound = nonSampledValuesPerRound + sampledValuesPerRound;
         tsInput = simulateSDtransfer(tsInput, valuesPerRound, nWaitingUnits, option);
-        tsOutput = simulateVout(tsInput);
-        tsInjPower = timeseries((dampingRate.* (tsInput.Data .* tsOutput.Data)), tsOutput.Time);
-        tsInjPower.Name = 'injectedPower';
-        tsInjPower.DataInfo.Units = 'V^2 Hz';
-        tsCollection = tscollection({tsInput, tsOutput, tsInjPower});
+        tsOutput = simulateVout(tsInput);  
+        [vIn, vOut, vR, iR, iC, pR, pC, pIn, deltaPower] = ...
+            makeCircuitVariablesFromTimeSeries(tsInput, tsOutput, R_ohm, C_farads);
+        
+        [langevinDissipated, langevinStored, langevinInjected, langevinDelta ] = ...
+            makeLangevinVariablesFromTimeSeries( vIn, vOut, R_ohm, C_farads );
+        
+        tsCollection = tscollection({vIn, vOut, vR, iR, iC, pR, pC, pIn, deltaPower,...
+            langevinDissipated, langevinInjected, langevinStored, langevinDelta});
         tsCollection.Name = strcat( 'tscOption1_', num2str(freqSignalHz),'Hz');
         
     case'option2'
@@ -81,11 +90,15 @@ switch option
         nWaitingUnits = Parameters.nWaitingUnits;
         valuesPerRound = sampledValuesPerRound;
         tsInput = simulateSDtransfer(tsInput, valuesPerRound, nWaitingUnits, option);
-        tsOutput = simulateVout(tsInput);
-        tsInjPower = timeseries((dampingRate.* (tsInput.Data .* tsOutput.Data)), tsOutput.Time);
-        tsInjPower.Name = 'injectedPower';
-        tsInjPower.DataInfo.Units = 'V^2 Hz';
-        tsCollection = tscollection({tsInput, tsOutput, tsInjPower});
+        tsOutput = simulateVout(tsInput); 
+        [vIn, vOut, vR, iR, iC, pR, pC, pIn, deltaPower] = ...
+            makeCircuitVariablesFromTimeSeries(tsInput, tsOutput, R_ohm, C_farads);
+        
+        [langevinDissipated, langevinStored, langevinInjected, langevinDelta ] = ...
+            makeLangevinVariablesFromTimeSeries( vIn, vOut, R_ohm, C_farads );
+        
+        tsCollection = tscollection({vIn, vOut, vR, iR, iC, pR, pC, pIn, deltaPower,...
+            langevinDissipated, langevinInjected, langevinStored, langevinDelta});
         tsCollection.Name = strcat( 'tscOption2_', num2str(freqSignalHz),'Hz');
         
     case'option3'
@@ -107,10 +120,15 @@ switch option
         valuesPerRound = sampledValuesPerRound;
         tsInput = simulateSDtransfer(tsInput, valuesPerRound, nWaitingUnits, option);
         tsOutput = simulateVout(tsInput);
-        tsInjPower = timeseries((dampingRate.* (tsInput.Data .* tsOutput.Data)), tsOutput.Time);
-        tsInjPower.Name = 'injectedPower';
-        tsInjPower.DataInfo.Units = 'V^2 Hz';
-        tsCollection = tscollection({tsInput, tsOutput, tsInjPower});
+        
+        [vIn, vOut, vR, iR, iC, pR, pC, pIn, deltaPower] = ...
+            makeCircuitVariablesFromTimeSeries(tsInput, tsOutput, R_ohm, C_farads);
+        
+        [langevinDissipated, langevinStored, langevinInjected, langevinDelta ] = ...
+            makeLangevinVariablesFromTimeSeries( vIn, vOut, R_ohm, C_farads );
+        
+        tsCollection = tscollection({vIn, vOut, vR, iR, iC, pR, pC, pIn, deltaPower,...
+            langevinDissipated, langevinInjected, langevinStored, langevinDelta});
         tsCollection.Name = strcat( 'tscoption3_', num2str(freqSignalHz),'Hz');
         
     case'option1+3'
@@ -133,11 +151,14 @@ switch option
         valuesPerRound = nonSampledValuesPerRound + sampledValuesPerRound;
         tsInput = simulateSDtransfer(tsInput, valuesPerRound, nWaitingUnits, 'option3');
         tsOutput = simulateVout(tsInput);
-        tsInjPower = timeseries((dampingRate.* (tsInput.Data .* tsOutput.Data)),...
-            tsOutput.Time);
-        tsInjPower.Name = 'injectedPower';
-        tsInjPower.DataInfo.Units = 'V^2 Hz';
-        tsCollection = tscollection({tsInput, tsOutput, tsInjPower});
+        [vIn, vOut, vR, iR, iC, pR, pC, pIn, deltaPower] = ...
+            makeCircuitVariablesFromTimeSeries(tsInput, tsOutput, R_ohm, C_farads);
+        
+        [langevinDissipated, langevinStored, langevinInjected, langevinDelta ] = ...
+            makeLangevinVariablesFromTimeSeries( vIn, vOut, R_ohm, C_farads );
+        
+        tsCollection = tscollection({vIn, vOut, vR, iR, iC, pR, pC, pIn, deltaPower,...
+            langevinDissipated, langevinInjected, langevinStored, langevinDelta});
         tsCollection.Name = strcat( 'tscOption1+3Full_', num2str(freqSignalHz),'Hz');
         
     otherwise
@@ -152,18 +173,18 @@ varargout{1}.maxVin = max(tsCollection.Vin.Data);
 varargout{1}.minVin = min(tsCollection.Vin.Data);
 varargout{1}.maxVout = max(tsCollection.Vout.Data);
 varargout{1}.minVout = max(tsCollection.Vout.Data);
-varargout{1}.minPower = min(tsCollection.injectedPower.Data);
-varargout{1}.maxPower = max(tsCollection.injectedPower.Data);
+varargout{1}.minPower = min(tsCollection.Pin.Data);
+varargout{1}.maxPower = max(tsCollection.Pin.Data);
 varargout{1}.fsignal = freqSignalHz;
 varargout{1}.tsc = tsCollection;
 
 %% filtered/sampled signals
-[tsSampledVin, tsSampledVout] = filterMemSDSimulation(tsInput, tsOutput, ...
+[tsSampledVin, tsSampledVout] = filterMemSDSimulation(vIn, vOut, ...
     sampledValuesPerRound, nonSampledValuesPerRound, nWaitingUnits);
 tsSampledPower = timeseries((dampingRate.* (tsSampledVin.Data .* ...
     tsSampledVout.Data)), tsSampledVout.Time);
-tsSampledPower.Name = tsInjPower.Name;
-tsSampledPower.DataInfo.Units = tsInjPower.DataInfo.Units;
+tsSampledPower.Name = pIn.Name;
+tsSampledPower.DataInfo.Units = pIn.DataInfo.Units;
 tsSampledCollection = tscollection({tsSampledVin, tsSampledVout, tsSampledPower});
 tsSampledCollection.Name = strcat( 'tscOption1+3Sampled_', num2str(freqSignalHz),'Hz');
 varargout{2}.fsignal = freqSignalHz;
@@ -173,7 +194,9 @@ varargout{2}.maxVin = max(tsCollection.Vin.Data);
 varargout{2}.minVin = min(tsCollection.Vin.Data);
 varargout{2}.maxVout = max(tsCollection.Vout.Data);
 varargout{2}.minVout = min(tsCollection.Vout.Data);
-varargout{2}.minPower = min(tsSampledCollection.injectedPower.Data);
-varargout{2}.maxPower = max(tsSampledCollection.injectedPower.Data);
+varargout{2}.minPower = min(tsSampledCollection.Pin.Data);
+varargout{2}.maxPower = max(tsSampledCollection.Pin.Data);
+varargout{2}.maxPowerLangevin = max(tsCollection.langevinInjected.Data);
+varargout{2}.minPowerLangevin = min(tsCollection.langevinInjected.Data);
 
 end
