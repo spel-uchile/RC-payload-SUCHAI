232be54d76763828d2f8ccd8122e10220fa65f08
joseogalde <jose.ogalde@ing.uchile.cl>
2018-01-05 15:05:42 -0300

    added a function that computes power injection, stored power and dissipated power according to the Langevin equation

---
+++ suchai1/matlab/makeLangevinVariablesFromTimeSeries.m
@@ -0,0 +1,33 @@
+function [langevinDissipated, langevinStored, langevinInjected, langevinDelta] = ...
+    makeLangevinVariablesFromTimeSeries( vIn_ts, vOut_ts, R_ohm, C_farads )
+
+disp('making langevinDissipated, langevinStored, langevinInjected, langevinDelta');
+
+dampingRateHz = 1/ (R_ohm*C_farads);
+v2 = vOut_ts.Data.*vOut_ts.Data;
+t = vOut_ts.Time;
+dv2 = v2(2:end) - v2(1:end-1);
+dt = t(2:end) - t(1:end-1);
+dValue2 = dv2./dt;
+dValue2 = [0; dValue2]; % add a zero
+
+%% Langevin equation variables
+% Resistor
+dissFactorLangevin = v2.*dampingRateHz;
+langevinDissipated = timeseries(dissFactorLangevin,vOut_ts.Time,'name','langevinDissipated');
+langevinDissipated.DataInfo.Units = 'V^2 Hz';
+
+% Capacitor
+langevinStored = timeseries((dValue2)./2, vOut_ts.Time,'name', 'langevinStored');
+langevinStored.DataInfo.Units = 'V^2 Hz';
+
+%Injected Power
+langevinInjected = timeseries(dampingRateHz.*(vIn_ts.Data .* vOut_ts.Data),...
+    vOut_ts.Time, 'name', 'langevinInjected');
+langevinInjected.DataInfo.Units = 'V^2 Hz';
+
+langevinDelta = timeseries((langevinInjected.Data - (langevinDissipated.Data + langevinStored.Data)),...
+    vOut_ts.Time, 'name', 'langevinDeltaPower');
+langevinDelta.DataInfo.Units = 'V^2 Hz';
+end
+
