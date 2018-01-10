ea18366e3da1f2e152cb47bce8fdeb8959aa68c0
joseogalde <jose.ogalde@ing.uchile.cl>
2018-01-10 01:00:03 -0300

    added a small (very small) epsilon value to the PDF when it reaches zero, only no to see a -Inf in log scale plots

--- suchai1/matlab/normalize.m
+++ suchai1/matlab/normalize.m
@@ -1,3 +1,5 @@
 function normalizedData = normalize( supportVector, data)
-normalizedData = data./trapz(supportVector, data);
+    normalizedData = data./trapz(supportVector, data);
+    smallValuesIdx = normalizedData <= eps;
+    normalizedData(smallValuesIdx) = eps;
 end
