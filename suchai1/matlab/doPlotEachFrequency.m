d5d3f1b469d4b4d27d97381bb3b538b3a684c3e9
joseogalde <jose.ogalde@ing.uchile.cl>
2018-01-11 18:21:07 -0300

    refactor of the PDF plotting script

--- suchai1/matlab/doPlotEachFrequency.m
+++ suchai1/matlab/doPlotEachFrequency.m
@@ -1,7 +1,18 @@
 clear all;
 close all;
 
-rootDir= './mat/pdf-bwauto';
+global matfileTM;
+global matfileLab;
+global suchaiFoldersName;
+global saveFolder;
+global mkrsize,
+global freqsLab;
+global freqsLegendTM;
+global tmPerFreqSuchai;
+global freqsLegendLab;
+global tmPerFreqLab;
+
+rootDir= './mat/pdf-bwmanual2';
 saveFolder =['./img/suchaiPDFs/SeparatedByFrequency/',date];
 mkrsize = 6;
 myLegendFontSize = 10;
@@ -16,7 +27,7 @@ suchaiFoldersStruct = suchaiFoldersStruct(3:end);
 isADir = [suchaiFoldersStruct.isdir];
 suchaiFoldersStruct = suchaiFoldersStruct(isADir);
 suchaiFoldersName = {suchaiFoldersStruct.name};
-suchaiFoldersName = sortn(suchaiFoldersName);
+suchaiFoldersName = sortn(suchaiFoldersName)
 freqsTelemetry = {};
 freqsLegendTM = {};
 telemetryCounter = 0;
@@ -69,189 +80,16 @@ for i = 1 : numel(suchaiFoldersName)
 end
 plotLegendCell = [freqsLegendTM, freqsLegendLab]';
 
-%% Pin Plot
-ks = 1;
-kl = 1;
-subFolderName = 'Pin';
-saveFolderFig = [saveFolder , '/', subFolderName];
-if ~isdir(saveFolderFig)
-    mkdir(saveFolderFig);
-end
-for i = 1 : numel(suchaiFoldersName)
-    hfig = figure('units','normalized','outerposition',[0 0 1 1]);
-    currLegendSuchai = freqsLegendTM(ks:ks+tmPerFreqSuchai(i)-1);
-    for j = 1 : tmPerFreqSuchai(i)
-        xbins = matfileTM{ks}.xbins;
-        pdfResult = matfileTM{ks}.pdfResult;
-        Parameters = matfileTM{ks}.Parameters;
-        hold on;
-        plot(xbins.raw.Pin, log10(pdfResult.raw.Pin),'*','MarkerSize', mkrsize);
-        hold off;
-        ks = ks +1;
-    end
-    currLegendLab = freqsLegendLab(kl:kl+tmPerFreqLab(i)-1);
-    for j = 1 : tmPerFreqLab(i)
-        xbins = matfileLab{kl}.xbins;
-        pdfResult = matfileLab{kl}.pdfResult;
-        Parameters = matfileLab{kl}.Parameters;
-        hold on;
-        plot(xbins.raw.Pin, log10(pdfResult.raw.Pin),'o','MarkerSize', mkrsize);
-        hold off;
-        kl = kl + 1;
-    end
-    grid on;
-    ylim([-3 1]);
-    xlim([-0.4 1.5]);
-    yt = get(gca, 'YTick');
-    myylabels = cellstr(num2str(yt(:), '10^{%.1f}'));
-    set(gca,'YTickLabel', myylabels);
-    set(gca, 'YMinorTick','on', 'YMinorGrid','on');
-    title(['Power Input PDF ', num2str(str2double(freqsLab(i))/91.5), ' f_{RC}']);
-    xlabel('mW');
-    currLegend = [currLegendSuchai, currLegendLab]';
-    hleg = legend(currLegend,'Location','eastoutside','Orientation','vertical');
-    set(hleg, 'Interpreter', 'none');
-    figSaveName = [saveFolderFig, '/',suchaiFoldersName{i},'_Pin_',date,'.png'];
-    saveas(gcf, figSaveName);
-end
-close all;
-
-%% Pr Plot
-ks = 1;
-kl = 1;
-subFolderName = 'Pr';
-saveFolderFig = [saveFolder , '/', subFolderName];
-if ~isdir(saveFolderFig)
-    mkdir(saveFolderFig);
-end
-for i = 1 : numel(suchaiFoldersName)
-    hfig = figure('units','normalized','outerposition',[0 0 1 1]);
-    currLegendSuchai = freqsLegendTM(ks:ks+tmPerFreqSuchai(i)-1);
-    for j = 1 : tmPerFreqSuchai(i)
-        xbins = matfileTM{ks}.xbins;
-        pdfResult = matfileTM{ks}.pdfResult;
-        Parameters = matfileTM{ks}.Parameters;
-        hold on;
-        plot(xbins.raw.Pr, log10(pdfResult.raw.Pr),'*','MarkerSize', mkrsize);
-        hold off;
-        ks = ks +1;
-    end
-    currLegendLab = freqsLegendLab(kl:kl+tmPerFreqLab(i)-1);
-    for j = 1 : tmPerFreqLab(i)
-        xbins = matfileLab{kl}.xbins;
-        pdfResult = matfileLab{kl}.pdfResult;
-        Parameters = matfileLab{kl}.Parameters;
-        hold on;
-        plot(xbins.raw.Pr, log10(pdfResult.raw.Pr),'o','MarkerSize', mkrsize);
-        hold off;
-        kl = kl + 1;
-    end
-    grid on;
-    ylim([-5 1]);
-    xlim([-0.5 3]);
-    yt = get(gca, 'YTick');
-    myylabels = cellstr(num2str(yt(:), '10^{%.1f}'));
-    set(gca,'YTickLabel', myylabels);
-    set(gca, 'YMinorTick','on', 'YMinorGrid','on');
-    title(['Power Resistor PDF', num2str(str2double(freqsLab(i))/91.5), ' f_{RC}']);
-    xlabel('mW');
-    currLegend = [currLegendSuchai, currLegendLab]';
-    hleg = legend(currLegend,'Location','eastoutside','Orientation','vertical');
-    set(hleg, 'Interpreter', 'none');
-    figSaveName = [saveFolderFig, '/',suchaiFoldersName{i},'_Pr_',date,'.png'];
-    saveas(gcf, figSaveName);
-end
-close all;
-
-%% LangInj Plot
-ks = 1;
-kl = 1;
-subFolderName = 'LangInj';
-saveFolderFig = [saveFolder , '/', subFolderName];
-if ~isdir(saveFolderFig)
-    mkdir(saveFolderFig);
-end
-for i = 1 : numel(suchaiFoldersName)
-    hfig = figure('units','normalized','outerposition',[0 0 1 1]);
-    currLegendSuchai = freqsLegendTM(ks:ks+tmPerFreqSuchai(i)-1);
-    for j = 1 : tmPerFreqSuchai(i)
-        xbins = matfileTM{ks}.xbins;
-        pdfResult = matfileTM{ks}.pdfResult;
-        Parameters = matfileTM{ks}.Parameters;
-        hold on;
-        plot(xbins.raw.LangInj, log10(pdfResult.raw.LangInj),'*','MarkerSize', mkrsize);
-        hold off;
-        ks = ks +1;
-    end
-    currLegendLab = freqsLegendLab(kl:kl+tmPerFreqLab(i)-1);
-    for j = 1 : tmPerFreqLab(i)
-        xbins = matfileLab{kl}.xbins;
-        pdfResult = matfileLab{kl}.pdfResult;
-        Parameters = matfileLab{kl}.Parameters;
-        hold on;
-        plot(xbins.raw.LangInj, log10(pdfResult.raw.LangInj),'o','MarkerSize', mkrsize);
-        hold off;
-        kl = kl + 1;
-    end
-    grid on;
-    ylim([-4 -1]);
-    xlim([-350 550]);
-    yt = get(gca, 'YTick');
-    myylabels = cellstr(num2str(yt(:), '10^{%.1f}'));
-    set(gca,'YTickLabel', myylabels);
-    set(gca, 'YMinorTick','on', 'YMinorGrid','on');
-    title(['Injected Power PDF ', num2str(str2double(freqsLab(i))/91.5), ' f_{RC}']);
-    xlabel('V^2 \cdot Hz');
-    currLegend = [currLegendSuchai, currLegendLab]';
-    hleg = legend(currLegend,'Location','eastoutside','Orientation','vertical');
-    set(hleg, 'Interpreter', 'none');
-    figSaveName = [saveFolderFig, '/',suchaiFoldersName{i},'_LangInj_',date,'.png'];
-    saveas(gcf, figSaveName);
-end
-
-%% LangDiss Plot
-ks = 1;
-kl = 1;
-subFolderName = 'LangDiss';
-saveFolderFig = [saveFolder , '/', subFolderName];
-if ~isdir(saveFolderFig)
-    mkdir(saveFolderFig);
-end
-for i = 1 : numel(suchaiFoldersName)
-    hfig = figure('units','normalized','outerposition',[0 0 1 1]);
-    currLegendSuchai = freqsLegendTM(ks:ks+tmPerFreqSuchai(i)-1);
-    for j = 1 : tmPerFreqSuchai(i)
-        xbins = matfileTM{ks}.xbins;
-        pdfResult = matfileTM{ks}.pdfResult;
-        Parameters = matfileTM{ks}.Parameters;
-        hold on;
-        plot(xbins.raw.LangDiss, log10(pdfResult.raw.LangDiss),'*','MarkerSize', mkrsize);
-        hold off;
-        ks = ks +1;
-    end
-    currLegendLab = freqsLegendLab(kl:kl+tmPerFreqLab(i)-1);
-    for j = 1 : tmPerFreqLab(i)
-        xbins = matfileLab{kl}.xbins;
-        pdfResult = matfileLab{kl}.pdfResult;
-        Parameters = matfileLab{kl}.Parameters;
-        hold on;
-        plot(xbins.raw.LangDiss, log10(pdfResult.raw.LangDiss),'o','MarkerSize', mkrsize);
-        hold off;
-        kl = kl + 1;
-    end
-    grid on;
-    ylim([-4 -1]);
-    xlim([-150 650]);
-    yt = get(gca, 'YTick');
-    myylabels = cellstr(num2str(yt(:), '10^{%.1f}'));
-    set(gca,'YTickLabel', myylabels);
-    set(gca, 'YMinorTick','on', 'YMinorGrid','on');
-    title(['Dissipated Power PDF ', num2str(str2double(freqsLab(i))/91.5), ' f_{RC}']);
-    xlabel('V^2 \cdot Hz');
-    currLegend = [currLegendSuchai, currLegendLab]';
-    hleg = legend(currLegend,'Location','eastoutside','Orientation','vertical');
-    set(hleg, 'Interpreter', 'none');
-    figSaveName = [saveFolderFig, '/',suchaiFoldersName{i},'_LangDiss_',date,'.png'];
-    saveas(gcf, figSaveName);
-end
-close all;
+plotPDFVariable('Vin', [-1.2 1.2],[-3 1],'V');
+plotPDFVariable('Vout', [-1.2 1.2],[-3 1],'V');
+plotPDFVariable('Vr', [-1.6 1.6],[-3 1],'V');
+plotPDFVariable('Ir', [-1.6 1.6],[-3 1],'mA');
+plotPDFVariable('Ic', [-0.3 0.3],[-3 2],'mA');
+plotPDFVariable('Pin', [-0.4 1.5],[-3 1],'mW');
+plotPDFVariable('Pr', [-0.5 3],[-5 1],'mW');
+plotPDFVariable('Pc', [-0.25 1.25],[-5 2],'mW');
+plotPDFVariable('DeltaP', [-0.25 1.25],[-5 2],'mW');
+plotPDFVariable('LangInj', [-350 550],[-4 -1],'V^2 \cdot Hz');
+plotPDFVariable('LangDiss',[-150 650],[-4 -1],'V^2 \cdot Hz')
+plotPDFVariable('LangStored', [-300 300],[-4 -0.5],'V^2 \cdot Hz');
+plotPDFVariable('LangDeltaP',[-200 650],[-4 -0.5],'V^2 \cdot Hz')
