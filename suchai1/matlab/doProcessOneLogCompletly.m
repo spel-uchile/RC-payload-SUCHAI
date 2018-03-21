d5fc88ded2d055f97f48d68d0857464844ddf5b5
joseogalde <jose.ogalde@ing.uchile.cl>
2018-03-21 15:01:29 -0300

    added a example to the doProcessOneLogCompletly.m in the case you want to process two or more telemetries

--- suchai1/matlab/doProcessOneLogCompletly.m
+++ suchai1/matlab/doProcessOneLogCompletly.m
@@ -1,3 +1,4 @@
+clear all;
 disp('Processing a new SUCHAI telemetry');
 
 %% when running for the first time uncommnet this values
@@ -12,3 +13,18 @@ end
 doParser;
 doTimeSeriesFactory;
 doPdfEstimator;
+
+%% Example: four telemetries processed
+% for prefixx = {'2018_01_17_123317','2018_01_23_013627','2018_01_23_030957','2018_01_24_132449'};
+%     prefix = prefixx{1};
+%     dataset = 'suchai';
+%     overwrite = 'yes';
+%     if strfind(dataset,'suchai');
+%         doPreProcessorSuchaiLogs;
+%     else
+%         doPreProcessor;
+%     end
+%     doParser;
+%     doTimeSeriesFactory;
+%     doPdfEstimator;
+% end
