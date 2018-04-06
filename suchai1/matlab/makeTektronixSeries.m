ec77a6a496fda5ad2e72dbe9b882de8a50fed67a
joseogalde <jose.ogalde@ing.uchile.cl>
2018-04-06 12:12:18 -0300

    remove the normalization lines of code, there will be a function/script that will perform this action

--- suchai1/matlab/makeTektronixSeries.m
+++ suchai1/matlab/makeTektronixSeries.m
@@ -7,21 +7,21 @@ end
 %% Measurements (Vin and Vout)
 disp(strcat('makeTektronixSeries Vin noise of bandwith ', num2str(freqSignalHz/1e6), ' MHz'));
 vIn_ts = timeseries(ch1, t,'name', 'Vin');
-vIn_ts.Data = vIn_ts.Data - mean(vIn_ts.Data);
+% vIn_ts.Data = vIn_ts.Data - mean(vIn_ts.Data);
 vIn_ts.DataInfo.Units = 'V';
 vIn_ts.DataInfo.Interpolation = tsdata.interpolation('zoh');
 vIn_ts.TimeInfo.Units = 'seconds';
 
 disp(strcat('makeTektronixSeries Vout with freq ', num2str(freqSignalHz/1e6), 'MHz'));
 vOut_ts = timeseries(ch2, t,'name', 'Vout');
-vOut_ts.Data = vOut_ts.Data - mean(vOut_ts.Data);
+% vOut_ts.Data = vOut_ts.Data - mean(vOut_ts.Data);
 vOut_ts.DataInfo.Units = 'V';
 vOut_ts.DataInfo.Interpolation = tsdata.interpolation('zoh');
 vOut_ts.TimeInfo.Units = 'seconds';
 
 disp(strcat('makeTektronixSeries  Vin x Vout (MATH) with freq ', num2str(freqSignalHz/1e6), ' MHz'));
 math_ts = timeseries(vIn_ts.Data.*vOut_ts.Data, t, 'name', 'Math');
-math_ts.Data = math_ts.Data - mean(math_ts.Data);
+% math_ts.Data = math_ts.Data - mean(math_ts.Data);
 math_ts.DataInfo.Units = 'mV^2';
 math_ts.DataInfo.Interpolation = tsdata.interpolation('zoh');
 math_ts.TimeInfo.Units = 'seconds';
