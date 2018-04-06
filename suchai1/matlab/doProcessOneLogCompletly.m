f9ed4807b45528d253cd6c5ade6d9b48608b1de2
joseogalde <jose.ogalde@ing.uchile.cl>
2018-04-06 12:21:50 -0300

    changed the main script that process (parser, timeseries, pdf) all the telemetries between the three available datasets = 'lab', 'suchai' and 'tektronix'

--- suchai1/matlab/doProcessOneLogCompletly.m
+++ suchai1/matlab/doProcessOneLogCompletly.m
@@ -1,18 +1,37 @@
 clear all;
-disp('Processing a new SUCHAI telemetry');
+disp('Processing a telemetry');
 
 %% when running for the first time uncommnet this values
-% prefix = '2016_18_05';
-% dataset = 'lab';
-overwrite = 'yes';
-if strfind(dataset,'suchai');
-    doPreProcessorSuchaiLogs;
-else
-    doPreProcessor;
+datasets = {'suchai','tektronik','lab',};
+for i = 1 : length(datasets)
+    dataset = datasets{i};
+    seedFolder = ['./logs/',dataset];
+    datesOfExec = dir(seedFolder);
+    datesOfExec = {datesOfExec.name};
+    datesOfExec = datesOfExec(3:end);
+    datesOfExec = sortn(datesOfExec);
+    datesOfExec = lower(datesOfExec);
+    
+    for j = 1 : length(datesOfExec)
+        prefix = datesOfExec{j};
+        overwrite = 'yes';
+        if strfind(dataset,'suchai');
+            doPreProcessorSuchaiLogs;
+            doParser;
+            doTimeSeriesFactory;
+        elseif strfind(dataset,'tektronix');
+            doTimeSeriesTektronix;
+        else
+            doPreProcessor;
+            doParser;
+            doTimeSeriesFactory;
+        end
+    end
+    doPdfEstimator;
 end
-doParser;
-doTimeSeriesFactory;
-doPdfEstimator;
+%% Aleluya sound
+Data = load('handel.mat');
+sound(Data.y, Data.Fs)
 
 %% Example: four telemetries processed
 % for prefixx = {'2018_01_17_123317','2018_01_23_013627','2018_01_23_030957','2018_01_24_132449'};
--- suchai1/matlab/doTimeSeriesFactory.m
+++ suchai1/matlab/doTimeSeriesFactory.m
@@ -2,91 +2,68 @@
 % files and modifies and saves them in MAT-files for further time series
 % creation.
 
-% for prefixx = {prefix}
-% dataset = 'suchai'; %'lab' or 'suchai'
+% 'prefix' is an important variable that is a string with the date of the
+% telemtry to be processed e.g: '2018_03_07_150000'
+prefixjoin = [prefix(1:4), prefix(6:7), prefix(9:end)];
+parserFolder = ['./parser/', dataset];
+fixtureFolder = strcat(parserFolder,'/', prefix);
+parserFiles = dir(fixtureFolder);
+parserFiles = {parserFiles.name};
+parserFiles = parserFiles(3:end)';
+parserFiles = sortn(parserFiles);
 
-% IF YOU WANNA PROCESS ALL THE SUCHAI TELEMETRIES UNCOMMENT THIS BELOW
-for prefixx = {'2017_08_24_132346','2017_08_29_131900','2017_08_30_022511', ...
-        '2017_08_31_124200','2017_09_01_135700', '2017_09_05_020600',...
-        '2017_09_08_132000','2017_09_15_020900', '2017_09_16_032505',...
-        '2017_09_18_132118', '2017_09_22_030600',...
-        '2017_09_22_133600','2017_09_24_130307', '2017_09_26_135800', ...
-        '2017_09_27_133958','2017_09_28_024600', '2017_09_29_130300',...
-        '2017_10_13_024557','2017_10_12_133800','2017_09_18_132118',...
-        '2018_01_10_131222','2018_01_11_125344','2018_01_12_140716',...
-        '2018_01_13_031449','2018_01_14_132913','2018_01_15_023601',...
-        '2018_01_15_000001','2018_01_15_000002','2018_01_15_000003',...
-        '2018_01_15_000004','2018_01_17_123317'};
-
-    prefix = prefixx{1};
-    prefixjoin = [prefix(1:4), prefix(6:7), prefix(9:end)];
-    parserFolder = ['./parser/', dataset];
-    fixtureFolder = strcat(parserFolder,'/', prefix);
-    parserFiles = dir(fixtureFolder);
-    parserFiles = {parserFiles.name};
-    parserFiles = parserFiles(3:end)';
-    parserFiles = sortn(parserFiles);
+charIndexForInputFixture = strfind(lower(parserFiles), 'input');
+fixtureIndexForInputParserFiles = find(~cellfun(@isempty, charIndexForInputFixture));
+inputFixture = strcat(fixtureFolder,'/', parserFiles{fixtureIndexForInputParserFiles});
+outputFixture = cell(1, length(parserFiles) - 1);
+fixtureIndexForOutputFiles = 0;
+for i = 1 : length(parserFiles)
+    fixtureIndexForOutputFiles = fixtureIndexForOutputFiles + 1;
     
-    charIndexForInputFixture = strfind(lower(parserFiles), 'input');
-    fixtureIndexForInputParserFiles = find(~cellfun(@isempty, charIndexForInputFixture));
-    inputFixture = strcat(fixtureFolder,'/', parserFiles{fixtureIndexForInputParserFiles});
-    outputFixture = cell(1, length(parserFiles) - 1);
-    fixtureIndexForOutputFiles = 0;
-    for i = 1 : length(parserFiles)
-        fixtureIndexForOutputFiles = fixtureIndexForOutputFiles + 1;
+    if i ~= fixtureIndexForInputParserFiles
+        outputFixture{fixtureIndexForOutputFiles} = strcat(fixtureFolder,'/',...
+            parserFiles{fixtureIndexForOutputFiles});
+        S = load(outputFixture{fixtureIndexForOutputFiles});
+        name = fieldnames(S);
+        adcPeriod = S.(name{1}).adcPeriod;
+        sampCoeff = S.(name{1}).oversamplingCoeff;
+        fsignal = computeFreqSignalHz(adcPeriod, sampCoeff);
+        saveFolder = strcat(['./mat/ts/',dataset,'/'], num2str(fsignal));
         
-        if i ~= fixtureIndexForInputParserFiles
-            outputFixture{fixtureIndexForOutputFiles} = strcat(fixtureFolder,'/',...
-                parserFiles{fixtureIndexForOutputFiles});
-            S = load(outputFixture{fixtureIndexForOutputFiles});
-            name = fieldnames(S);
-            adcPeriod = S.(name{1}).adcPeriod;
-            sampCoeff = S.(name{1}).oversamplingCoeff;
-            fsignal = computeFreqSignalHz(adcPeriod, sampCoeff);
-            saveFolder = strcat(['./mat/ts/',dataset,'/'], num2str(fsignal));
-            
-            if ~isdir(saveFolder)
-                mkdir(saveFolder)
-            end
-            
-            %raw timeserie
-            disp(['raw timeseries ', num2str(fsignal), 'Hz']);
-            raw = timeSeriesFactory(fsignal, 'raw', inputFixture, ...
-                outputFixture{fixtureIndexForOutputFiles}, sampCoeff);
-            newRawFile = strcat(saveFolder, '/',prefixjoin,'_', raw.Name,'.mat');
-            save(newRawFile,'raw','-v7.3');
-            
-            %filtered timeserie
-            disp(['filtered timeseries ', num2str(fsignal), 'Hz']);
-            filtered = timeSeriesFactory(fsignal, 'filtered', inputFixture,...
-                outputFixture{fixtureIndexForOutputFiles}, sampCoeff);
-            newFilteredFile = strcat(saveFolder, '/',prefixjoin,'_', filtered.Name,'.mat');
-            save(newFilteredFile,'filtered','-v7.3');
-            
-            %simulation timeserie
-            disp(['simulink timeseries ', num2str(fsignal), 'Hz']);
-            simulation = timeSeriesFactory(fsignal, 'simulink', inputFixture, ...
-                sampCoeff);
-            newSimFile = strcat(saveFolder, '/',prefixjoin,'_', simulation.Name,'.mat');
-            save(newSimFile,'simulation','-v7.3');
-            
-            %theoretical timeserie
-            disp(['theoretical simulation timeseries', num2str(fsignal), 'Hz']);
-            Parameters.numberOfRandomValues = 1e5;
-            Parameters.dacBits = 16;
-            Parameters.adcBits = 10;
-            Parameters.dacMaxVoltage = 3.3;
-            Parameters.dacMinvoltage = 0;
-            Parameters.oversamplingCoeff = 4;
-            Parameters.buffLen = 200;
-            Parameters.sampledValuesPerRound = floor(Parameters.buffLen / Parameters.oversamplingCoeff); %NOT USED REALLY
-            Parameters.rounds = 3;%NOT USED REALLY
-            Parameters.nWaitingUnits = 350; %NOT USED REALLY
-            Parameters.nonSampledValuesPerRound = 500;%NOT USED REALLY
-            theoretical = timeSeriesFactory(fsignal, 'theoretical', Parameters);
-            newTheoreticalFile = strcat(saveFolder, '/',prefixjoin,'_', theoretical.Name,'.mat');
-            save(newTheoreticalFile,'theoretical','-v7.3');
+        if ~isdir(saveFolder)
+            mkdir(saveFolder)
         end
         
+        %raw timeserie
+        disp(['raw timeseries ', num2str(fsignal), 'Hz']);
+        raw = timeSeriesFactory(fsignal, 'raw', inputFixture, ...
+            outputFixture{fixtureIndexForOutputFiles}, sampCoeff);
+        newRawFile = strcat(saveFolder, '/',prefixjoin,'_', raw.Name,'.mat');
+        save(newRawFile,'raw','-v7.3');
+        
+        %simulation timeserie
+        disp(['simulink timeseries ', num2str(fsignal), 'Hz']);
+        simulation = timeSeriesFactory(fsignal, 'simulink', inputFixture, ...
+            sampCoeff);
+        newSimFile = strcat(saveFolder, '/',prefixjoin,'_', simulation.Name,'.mat');
+        save(newSimFile,'simulation','-v7.3');
+        
+        %theoretical timeserie
+        disp(['theoretical simulation timeseries', num2str(fsignal), 'Hz']);
+        Parameters.numberOfRandomValues = 1e5;
+        Parameters.dacBits = 16;
+        Parameters.adcBits = 10;
+        Parameters.dacMaxVoltage = 3.3;
+        Parameters.dacMinvoltage = 0;
+        Parameters.oversamplingCoeff = 4;
+        Parameters.buffLen = 200;
+        Parameters.sampledValuesPerRound = floor(Parameters.buffLen / Parameters.oversamplingCoeff); %NOT USED REALLY
+        Parameters.rounds = 3;%NOT USED REALLY
+        Parameters.nWaitingUnits = 350; %NOT USED REALLY
+        Parameters.nonSampledValuesPerRound = 500;%NOT USED REALLY
+        theoretical = timeSeriesFactory(fsignal, 'theoretical', Parameters);
+        newTheoreticalFile = strcat(saveFolder, '/',prefixjoin,'_', theoretical.Name,'.mat');
+        save(newTheoreticalFile,'theoretical','-v7.3');
     end
+    
 end
